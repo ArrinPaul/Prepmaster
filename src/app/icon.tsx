e2717f0fc7be5fe84415cc407091ff54261ab14b
Arrin Paul <yarrinpaul11@gmail.com>
2025-11-23 23:13:10 +0530

    feat: add icon generation using ImageResponse for Open Graph

---
+++ src/app/icon.tsx
@@ -0,0 +1,39 @@
+import { ImageResponse } from 'next/og'
+ 
+// Route segment config
+export const runtime = 'edge'
+ 
+// Image metadata
+export const size = {
+  width: 32,
+  height: 32,
+}
+export const contentType = 'image/png'
+ 
+// Image generation
+export default function Icon() {
+  return new ImageResponse(
+    (
+      // ImageResponse JSX element
+      <div
+        style={{
+          fontSize: 24,
+          background: 'linear-gradient(90deg, #000 0%, #333 100%)',
+          width: '100%',
+          height: '100%',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          color: 'white',
+          fontWeight: 'bold',
+        }}
+      >
+        P
+      </div>
+    ),
+    // ImageResponse options
+    {
+      ...size,
+    }
+  )
+}
